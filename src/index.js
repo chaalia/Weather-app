070b397aa43891afba8fd223f36a013f2006b263
chaalia <rejeb.chaalia@gmail.com>
2021-10-13 16:47:07 +0100

    init in redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 
 ReactDOM.render(
   <React.StrictMode>
